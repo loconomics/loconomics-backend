2101fd00d44b86011fd994c2c566e248d1535f56
Nolan Darilek <nolan@thewordnerd.info>
2018-02-09 12:31:00 -0600

    URL of the backend is now configurable.

--- server/middleware/proxy.js
+++ server/middleware/proxy.js
@@ -1,8 +1,14 @@
+import url from "url"
+
 import httpProxy from "http-proxy"
 
 const proxy = httpProxy.createProxyServer()
 
-export default () => (req, res, next) => {
-  req.headers.host = "dev.loconomics.com"
-  proxy.web(req, res, {target: "http://dev.loconomics.com"})
+export default () => {
+  const backend = url.parse(process.env.LOCONOMICS_BACKEND_URL || "https://www.loconomics.com")
+  return (req, res, next) => {
+    req.headers.host = backend.host
+    console.log("Backend", backend)
+    proxy.web(req, res, {target: backend.href})
+  }
 }
