3ee33310a1aed5413d0ef449ac8c7b2f6fdb80b1
Nolan Darilek <nolan@thewordnerd.info>
2018-02-21 19:17:01 +0000

    Don't use LoopBack authentication for now.

--- server/boot/authentication.js
+++ server/boot/authentication.js
@@ -1,6 +1,4 @@
-'use strict';
-
-module.exports = function enableAuthentication(server) {
+export default (server) => {
   // enable authentication
-  server.enableAuth();
-};
+  //server.enableAuth()
+}
--- server/boot/initialize-database.js
+++ server/boot/initialize-database.js
@@ -1,8 +1,10 @@
 export default (app) => {
   const {db} = app.dataSources
+  /*
   db.automigrate("User")
   db.automigrate("AccessToken")
   db.automigrate("ACL")
   db.automigrate("RoleMapping")
   db.automigrate("Role")
+  */
 }
