962330f0bd04bf1617725a38be670c50bf0d0b65
Nolan Darilek <nolan@thewordnerd.info>
2018-03-08 17:15:28 +0000

    Add middleware to take URLs of the form /api/v1/..., strip the /v1 segment, and add the value to req.apiVersion.

--- server/middleware/versioner.js
+++ server/middleware/versioner.js
@@ -1,13 +1,13 @@
-import locale from "locale"
-
 export default (options) => {
-  const locales = new locale.Locales(options.locales)
+  const versionRegexp = /^\/api\/v([0-9+])\/.*/
   return (req, res, next) => {
-    if(req.path.startsWith("/api/")) {
+    const matches = req.path.match(versionRegexp)
+    if(matches) {
+      req.apiVersion = matches[1]
       const parts = req.path.split("/")
       const newPath = parts.slice(0, 2).concat(parts.slice(3)).join("/")
       req.url = newPath
     }
-    locale(locales)(req, res, next)
+    next()
   }
 }
