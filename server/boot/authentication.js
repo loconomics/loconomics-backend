8e5687c80e2d14715e73ce67adfe836c812c437d
Nolan Darilek <nolan@thewordnerd.info>
2018-03-09 18:31:12 +0000

    s/dev/development/

--- server/boot/authentication.js
+++ server/boot/authentication.js
@@ -2,7 +2,7 @@ export default (server) => {
   // enable authentication
   const {db} = server.dataSources
   // Don't touch the existing Users table in non-development environments for now.
-  if(process.env.NODE_ENV == "dev")
+  if(process.env.NODE_ENV == "development")
     db.automigrate("Member")
   db.automigrate("Token")
   db.automigrate("ACL")
