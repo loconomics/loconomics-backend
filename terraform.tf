8d564d060bdc99c28e303bb7fa16d8a7d88f2c3b
Nolan Darilek <nolan@thewordnerd.info>
2018-07-11 20:19:29 +0000

    Restore local copy of development database into newly-created instance.

--- terraform.tf
+++ terraform.tf
@@ -37,8 +37,7 @@ resource "random_string" "sql_server_password" {
   min_lower = 1
   number = true
   min_numeric = 1
-  special = true
-  min_special = 1
+  special = false
 }
 
 resource "azurerm_sql_server" "sql_server" {
@@ -50,13 +49,27 @@ resource "azurerm_sql_server" "sql_server" {
   administrator_login_password = "${random_string.sql_server_password.result}"
 }
 
+resource "azurerm_sql_firewall_rule" "nolan_home" {
+  name                = "NolanHome"
+  resource_group_name = "${azurerm_resource_group.app.name}"
+  server_name = "${azurerm_sql_server.sql_server.name}"
+  start_ip_address    = "70.0.0.0"
+  end_ip_address      = "71.0.0.0"
+}
+
 resource "azurerm_sql_database" "sql_server_database" {
   name                = "loconomics"
   location = "${azurerm_resource_group.app.location}"
   resource_group_name = "${azurerm_resource_group.app.name}"
   server_name = "${azurerm_sql_server.sql_server.name}"
+  depends_on = [
+    "azurerm_sql_firewall_rule.nolan_home",
+  ]
   provisioner "local-exec" {
     command = "mssql-scripter -U dev-loconomis -P ${var.sql_server_password} -S dev-loconomics.database.windows.net -d Dev >dev.sql"
+  }
+  provisioner "local-exec" {
+    command = "sqlcmd -S ${azurerm_sql_server.sql_server.fully_qualified_domain_name} -U ${azurerm_sql_server.sql_server.administrator_login} -P \"${azurerm_sql_server.sql_server.administrator_login_password}\" -i dev.sql"
   }
 }
 
