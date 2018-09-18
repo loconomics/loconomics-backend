ebf22a896b3ee3773a1c9565b8120cfd6aed93a9
Nolan Darilek <nolan@thewordnerd.info>
2018-09-18 15:38:17 -0500

    Implement postal-codes endpoint.

--- api/controllers/postal-codes.js
+++ api/controllers/postal-codes.js
@@ -19,6 +19,14 @@ module.exports = {
   },
   fn: async function(inputs, exits) {
     const {id} = inputs
-    return exits.notFound({errorMessage: "Postal Code Not Valid."})
+    const sql = await sails.helpers.mssql()
+    let data = await sql.query(`select * from postalcode where PostalCode = ${id}`)
+    const postalCode = data.recordset[0]
+    if(!postalCode)
+      return exits.notFound({errorMessage: "Postal Code Not Valid."})
+    data = await sql.query(`select * from stateprovince where StateProvinceID = ${postalCode.StateProvinceID}`)
+    const stateProvince = data.recordset[0]
+    const record = {city: postalCode.City, stateProvinceName: stateProvince.StateProvinceName, stateProvinceCode: stateProvince.StateProvinceCode}
+    return exits.success(record)
   }
 }
--- api/helpers/mssql.js
+++ api/helpers/mssql.js
@@ -1,8 +1,13 @@
 const sql = require('mssql')
 
+const pool = new sql.ConnectionPool(sails.config.custom.mssqlServerURL)
+  .connect()
+  .then((pool) => pool)
+
 module.exports = {
   fn: async function(inputs, exits) {
-    const pool = await sql.connect(sails.config.custom.mssqlServerURL)
-    return exits.success(pool)
+    const p = await pool
+    const request = new sql.Request(p)
+    return exits.success(request)
   }
 }
