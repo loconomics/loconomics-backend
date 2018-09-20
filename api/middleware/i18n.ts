09f401b5506d599d70217f031d7e78f55ee6a7b9
Nolan Darilek <nolan@thewordnerd.info>
2018-09-20 14:20:28 -0500

    Set `req.languageID`.

--- api/middleware/i18n.ts
+++ api/middleware/i18n.ts
@@ -6,9 +6,13 @@ export = async (req, res, next) => {
     const candidate = parts[3]
     if(candidate && !req.headers['accept-language'])
       req.headers['accept-language'] = candidate
-    const countryCode = req.headers['accept-language'].split('-')[1]
+    const split = req.headers['accept-language'].split('-')
+    const languageCode = split[0]
+    const countryCode = split[1]
     const sql = await sails.helpers.mssql()
-    const data = await sql.query(`select CountryID from country where CountryCodeAlpha2 = '${countryCode}'`)
+    let data = await sql.query(`select LanguageID from language where LanguageCode = '${languageCode}'`)
+    req.languageID = data.recordset[0].LanguageID
+    data = await sql.query(`select CountryID from country where CountryCodeAlpha2 = '${countryCode}'`)
     req.countryID = data.recordset[0].CountryID
     const newPath = parts.slice(0, 3).concat(parts.slice(4)).join('/')
     req.url = newPath
