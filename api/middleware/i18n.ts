3d9b64cd16044b8af19a5437b58c2d2f53519180
Nolan Darilek <nolan@thewordnerd.info>
2018-09-20 18:28:03 +0000

    Stash countryID in `req` for future lookups.

--- api/middleware/i18n.ts
+++ api/middleware/i18n.ts
@@ -1,11 +1,18 @@
-export = (req, res, next) => {
-  if(req.path.startsWith("/api/v1")) {
-    const parts = req.path.split("/")
+declare var sails: any;
+
+export = async (req, res, next) => {
+  if(req.path.startsWith('/api/v1')) {
+    const parts = req.path.split('/')
     const candidate = parts[3]
-    if(candidate && !req.headers["accept-language"])
-      req.headers["accept-language"] = candidate
-    const newPath = parts.slice(0, 3).concat(parts.slice(4)).join("/")
+    if(candidate && !req.headers['accept-language'])
+      req.headers['accept-language'] = candidate
+    const countryCode = req.headers['accept-language'].split('-')[1]
+    const sql = await sails.helpers.mssql()
+    const data = await sql.query(`select CountryID from country where CountryCodeAlpha2 = '${countryCode}'`)
+    req.countryID = data.recordset[0].CountryID
+    const newPath = parts.slice(0, 3).concat(parts.slice(4)).join('/')
     req.url = newPath
+    return next()
   }
   next()
 }
