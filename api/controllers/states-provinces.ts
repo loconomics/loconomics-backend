8fac8914454132b9c42121663ad22f24725a24a2
Nolan Darilek <nolan@thewordnerd.info>
2018-09-20 18:28:17 +0000

    Migrate /states-provinces endpoint.

---
+++ api/controllers/states-provinces.ts
@@ -0,0 +1,19 @@
+declare var sails: any;
+
+module.exports = {
+  friendlyName: 'States/pronvices lookup',
+  description: 'Lists states/provinces for the current country',
+  exits: {
+    success: {
+      outputExample: [
+        {
+        }
+      ]
+    }
+  },
+  fn: async function(inputs, exits) {
+    const sql = await sails.helpers.mssql()
+    const data = await sql.query(`select StateProvinceName as name, StateProvinceCode as code from stateprovince where CountryID = ${this.req.countryID}`)
+    return exits.success(data.recordset)
+  }
+}
