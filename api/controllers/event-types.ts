e15d835e00faba7e5690a967ea4d4a5192c1e183
Nolan Darilek <nolan@thewordnerd.info>
2018-09-21 12:36:50 -0500

    We can use inputs to grab query parameters. Nice.

--- api/controllers/event-types.ts
+++ api/controllers/event-types.ts
@@ -3,6 +3,11 @@ declare var sails: any;
 module.exports = {
   friendlyName: 'Event types lookup for calendar events',
   description: 'With the `OnlySelectable` query parameter, only returns event types that can be selected',
+  inputs: {
+    OnlySelectable: {
+      example: false
+    }
+  },
   exits: {
     success: {
       outputExample: [
@@ -17,7 +22,7 @@ module.exports = {
   fn: async function(inputs, exits) {
     const sql = await sails.helpers.mssql()
     let query = `select EventTypeID, EventType as InternalName, DisplayName from CalendarEventType`
-    if(this.req.query.OnlySelectable)
+    if(inputs.OnlySelectable)
       query += ` where DisplayName is not null`
     const data = await sql.query(query)
     return exits.success(data.recordset)
