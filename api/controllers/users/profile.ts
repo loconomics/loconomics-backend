3178cb426101d5974fef87e96af9a584992bd53d
Trevor Smith <trevorcarringtonsmith@gmail.com>
2018-10-12 08:20:33 -0600

    Migrate users/profile endpoint

---
+++ api/controllers/users/profile.ts
@@ -0,0 +1,130 @@
+declare var sails: any;
+
+module.exports = {
+  friendlyName: 'Fetchs user public profile information.',
+  description: 'Basic info as name (firstName, lastName,..), publicBio, \
+    contact data (email, phone), isFreelancer flag. \
+    When service professional, other fields are included as businessName, \
+    websiteUrl (is contact data), profileUrlSlug.',
+  inputs: {
+    id: {
+      example: "141",
+      required: true
+    }
+  },
+  exits: {
+    success: {
+      outputExample: {
+        businessName: "Loconomics",
+        email: "iago@loconomics.com",
+        firstName: "Iago",
+        isClient: true,
+        isOrganization: false,
+        isServiceProfessional: true,
+        lastName: "Lorenzo Salgueiro",
+        orgDescription: "Tools for small business owners",
+        orgName: "Loconomics",
+        orgWebsite: "https://dev.loconomics.com",
+        phone: "720-555-5555",
+        photoUrl: "https://dev.loconomics.com/en-US/Profile/Photo/141?v=2018-09-19T18:47:48",
+        publicBio: "My public bio.",
+        secondLastName: "",
+        serviceProfessionalProfileUrl: "https://dev.loconomics.com/-iagosrl",
+        serviceProfessionalProfileUrlSlug: "iagosrl",
+        serviceProfessionalWebsiteUrl: "https://dev.loconomics.com",
+        timeZone: "America/Los_Angeles",
+        updatedDate: "2018-09-19T18:47:48.637-07:00",
+        userID: 141
+      }
+    },
+    notFound: {
+      responseType: "notFound"
+    }
+  },
+  fn: async function(inputs, exits) {
+    const targetUserId = this.req.param('id')
+    let requesterUserId = -1
+    if(this.req.user){
+      requesterUserId = this.req.user.UserID
+    }
+    const sql = await sails.helpers.mssql()
+    let profileData = await sql.query(`SELECT TOP 1
+                -- ID
+                Users.userID
+                -- Name
+                ,firstName
+                ,lastName
+                ,secondLastName
+                ,businessName
+                ,publicBio
+                -- User Type
+                ,isProvider as isServiceProfessional
+                ,isCustomer as isClient
+                ,CASE WHEN PC.Active = 1 OR ${targetUserId} = ${requesterUserId} THEN UP.email ELSE null END as Email
+                ,CASE WHEN PC.Active = 1 OR ${targetUserId} = ${requesterUserId} THEN Users.MobilePhone ELSE null END As phone
+                ,CASE WHEN PC.Active = 1 OR ${targetUserId} = ${requesterUserId} THEN providerWebsiteUrl ELSE null END as serviceProfessionalWebsiteUrl
+                ,providerProfileUrl as serviceProfessionalProfileUrlSlug
+                ,cpa.timeZone as timeZone
+                ,users.isOrganization as isOrganization
+                ,org.orgName as orgName
+                ,org.orgDescription as orgDescription
+                ,org.orgWebsite as orgWebsite
+                ,Users.updatedDate
+            FROM Users
+                    INNER JOIN
+                UserProfile As UP
+                    ON UP.UserID = Users.UserID
+                    LEFT JOIN
+                ServiceProfessionalClient As PC
+                    ON
+                    (   PC.ServiceProfessionalUserID = ${targetUserId} AND PC.ClientUserID = ${requesterUserId}
+                     OR PC.ServiceProfessionalUserID = ${requesterUserId} AND PC.ClientUserID = ${targetUserId} )
+                    LEFT JOIN
+                CalendarProviderAttributes as cpa
+                    ON cpa.UserID = Users.UserID
+                    LEFT JOIN
+                userOrganization as org
+                    ON org.userID = Users.userID
+            WHERE Users.UserID = ${targetUserId}
+                -- Users must be active (no deleted and publicly active) OR to exist in relationship with the other user (active or not, but with record)
+              AND (Users.Active = 1 AND Users.AccountStatusID = 1 OR PC.Active is not null)`)
+    const userProfile = profileData.recordset[0]
+    const updatedDate = new Date(userProfile.updatedDate).toISOString()
+    // Build photo URL
+    const photoUrl = this.req.baseUrl.toString()
+      + '/' + this.req.headers['accept-language']
+      + '/Profile/Photo/' + targetUserId + '?v=' + updatedDate.split('.')[0]
+    // Build serviceProfessionalProfileUrl from serviceProfessionalProfileUrlSlug
+    // TODO Implement ASP.LcHelpers.StringSlugify() to make consistent slugs
+    let serviceProfessionalProfileUrl = ''
+    const customUrlPrefix = '-'
+    if (userProfile.serviceProfessionalProfileUrlSlug) {
+      serviceProfessionalProfileUrl = this.req.baseUrl.toString() + '/'
+        + customUrlPrefix + userProfile.serviceProfessionalProfileUrlSlug
+    }
+    const record = {
+      businessName: userProfile.businessName,
+      email: userProfile.Email,
+      firstName: userProfile.firstName,
+      isClient: userProfile.isClient,
+      isOrganization: userProfile.isOrganization,
+      isServiceProfessional: userProfile.isServiceProfessional,
+      lastName: userProfile.lastName,
+      orgDescription: userProfile.orgDescription,
+      orgName: userProfile.orgName,
+      orgWebsite: userProfile.orgWebsite,
+      phone: userProfile.phone,
+      photoUrl: photoUrl,
+      publicBio: userProfile.publicBio,
+      secondLastName: userProfile.secondLastName,
+      serviceProfessionalProfileUrl: serviceProfessionalProfileUrl,
+      serviceProfessionalProfileUrlSlug: userProfile.serviceProfessionalProfileUrlSlug,
+      serviceProfessionalWebsiteUrl: userProfile.serviceProfessionalWebsiteUrl,
+      timeZone: userProfile.timeZone,
+      updatedDate: updatedDate.split('Z')[0] + '-07:00',
+      userID: userProfile.UserID
+    }
+    sails.log('userProfile: ' + JSON.stringify(userProfile))
+    return exits.success(record)
+  }
+}
