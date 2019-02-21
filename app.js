d757bd1570d280ed1be0d87a481538605d8f5a0b
Nolan Darilek <nolan@thewordnerd.info>
2019-02-21 16:21:11 -0600

    Pull in @loconomics/data as a git submodule, set up module resolution to work, and add the data directory to the reload watch list.

--- app.js
+++ app.js
@@ -22,6 +22,7 @@
 
 require("reflect-metadata")
 require('ts-node/register');
+require('tsconfig-paths/register');
 
 // Ensure we're in the project directory, so cwd-relative paths work as expected
 // no matter where we actually lift from.
